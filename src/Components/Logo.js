7575df0a171905eb1ceac80b89899cbc0f3abb87
Regisl33 <reg33gaming@gmail.com>
2023-12-11 22:14:47 -0500

    started logo

---
+++ src/Components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./img/earth.png" alt="earth-logo" />
+      <h1>React Country App</h1>
+      <img src="./img/react.png" alt="react-logo" />
+    </div>
+  );
+};
+
+export default Logo;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from "../Components/Logo";
 
 const Home = () => {
   return (
-    <div>
-      <h1>TEST</h1>
+    <div className="home">
+      <Logo />
     </div>
   );
 };
