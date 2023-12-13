195e5f1232fff730248e3145f849d060605cb66e
Regisl33 <reg33gaming@gmail.com>
2023-12-12 19:15:11 -0500

    added navigation

--- src/Pages/CountriesDisplay.js
+++ src/Pages/CountriesDisplay.js
@@ -1,14 +1,22 @@
 import React, { useContext } from "react";
 import { ContextCountriesData } from "../Contexts/ContextCountriesData";
 import Country from "../Components/Country";
+import Logo from "../Components/Logo";
+import Navigation from "../Components/Navigation";
 
 const CountriesDisplay = () => {
   const data = useContext(ContextCountriesData);
   return (
-    <div className="displayCountries">
-      {data &&
-        data.map((country, index) => <Country key={index} country={country} />)}
-    </div>
+    <main>
+      <Logo />
+      <Navigation />
+      <div className="displayCountries">
+        {data &&
+          data.map((country, index) => (
+            <Country key={index} country={country} />
+          ))}
+      </div>
+    </main>
   );
 };
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../Components/Logo";
+import Navigation from "../Components/Navigation";
 
 const Home = () => {
   return (
     <div className="home">
       <Logo />
+      <Navigation />
     </div>
   );
 };
