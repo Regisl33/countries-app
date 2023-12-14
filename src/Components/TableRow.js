6e528be1f9e84fb2e14f2a22e4b5fe1b7b40622e
Regisl33 <reg33gaming@gmail.com>
2023-12-13 22:31:02 -0500

    table logic and style

--- src/Components/TableRow.js
+++ src/Components/TableRow.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const TableRow = ({ country, counter }) => {
+  const handleLanguages = () => {
+    let arrayLang = [];
+    for (const key in country.languages) {
+      arrayLang.push(country.languages[key]);
+    }
+    return arrayLang.length > 3
+      ? arrayLang.slice(0, 3).join(", ")
+      : arrayLang.join(", ");
+  };
   return (
     <tr className={counter}>
       <td>{counter}</td>
@@ -9,10 +18,16 @@ const TableRow = ({ country, counter }) => {
       </td>
       <td>{country.name.common}</td>
       <td>{country.capital}</td>
-      <td>{country.population}</td>
-      <td>{country.language}</td>
-      <td>{country.continent}</td>
-      <td>{country.timezones}</td>
+      <td>{country.population.toLocaleString().replaceAll(",", "")}</td>
+      <td>{handleLanguages()}</td>
+      <td>{country.continents.join(", ")}</td>
+      <td>
+        {country.timezones.length === 1
+          ? country.timezones[0]
+          : `From ${country.timezones[0]} to ${
+              country.timezones[country.timezones.length - 1]
+            }`}
+      </td>
     </tr>
   );
 };
