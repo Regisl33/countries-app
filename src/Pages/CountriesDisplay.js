87eb0ebaa7e45ed63109337dcc2c746ced3f139d
Regisl33 <reg33gaming@gmail.com>
2023-12-22 21:27:36 -0500

    home page done!

--- src/Pages/CountriesDisplay.js
+++ src/Pages/CountriesDisplay.js
@@ -8,19 +8,21 @@ const CountriesDisplay = () => {
   const data = useContext(ContextCountriesData);
   const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
   const [rangeValue, setRangeValue] = useState(48);
-  const [selectedContinent, setSelectedContinent] = useState(null);
-  const [mappedData, setMappedData] = useState([]);
+  const [selectedContinent, setSelectedContinent] = useState("");
   const [searchValue, setSearchValue] = useState("");
+  const [mappeddata, setMappedData] = useState([]);
 
   useEffect(() => {
     selectedContinent
       ? setMappedData(
           data.filter((country) =>
-            country.continent[0].includes(selectedContinent)
+            country.continents[0]
+              .toLowerCase()
+              .includes(selectedContinent.toLowerCase())
           )
         )
       : setMappedData(data);
-  }, []);
+  }, [selectedContinent, data]);
 
   return (
     <main>
@@ -49,19 +51,22 @@ const CountriesDisplay = () => {
                 type="radio"
                 id={continent}
                 name="radio"
+                checked={continent === selectedContinent}
                 onChange={(e) => setSelectedContinent(e.target.id)}
               />
               <label htmlFor={continent}>{continent}</label>
             </li>
           ))}
-          {selectedContinent && (
-            <button onClick={setSelectedContinent(null)}>Cancel</button>
-          )}
         </ul>
       </div>
+      {selectedContinent && (
+        <div className="cancel">
+          <button onClick={() => setSelectedContinent("")}>Cancel</button>
+        </div>
+      )}
       <div className="displayCountries">
-        {mappedData &&
-          mappedData
+        {mappeddata &&
+          mappeddata
             .filter((country) =>
               country.name.common.toLowerCase().includes(searchValue)
             )
